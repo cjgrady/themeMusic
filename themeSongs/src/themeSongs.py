02f341ed486b07aec51337ea6bbbb5d1002b83eb
CJ Grady <cjgrady@ku.edu>
2012-05-21 20:52:46 -0500

    Initial commit (testing configuration of git)

---
+++ themeSongs/src/themeSongs.py
@@ -0,0 +1,14 @@
+"""
+@summary: Main module for theme (entry) music.  Sits and waits for signals.
+@author: CJ Grady
+@version: 1.0
+@status: alpha
+"""
+
+if __name__ == '__main__':
+   # While not killed
+   # Poll texts
+   # Once one has been found
+   #  pause music
+   #  queue up theme song
+   pass
